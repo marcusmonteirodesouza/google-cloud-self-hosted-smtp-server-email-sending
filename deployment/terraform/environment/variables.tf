5974edd726984c8e101b968e6e5d0882651c9f39
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2023-01-30 13:41:44 -0500

    Fix environment/variables.tf

--- deployment/terraform/environment/variables.tf
+++ deployment/terraform/environment/variables.tf
@@ -18,3 +18,13 @@ variable "sendgrid_api_key" {
   description = "The SendGrid API key"
   sensitive   = true
 }
+
+variable "sendgrid_cloud_function_source_archive_bucket" {
+  type        = string
+  description = "The GCS bucket containing the zip archive which contains the SendGrid Cloud Function."
+}
+
+variable "sendgrid_cloud_function_source_archive_object" {
+  type        = string
+  description = "The source archive object (file) of the SendGrid Cloud Function in archive bucket."
+}
