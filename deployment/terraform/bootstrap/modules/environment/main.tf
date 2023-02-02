406fd7e7be9d467f3b927dade2139e07be4e5f1a
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2023-02-02 01:20:01 -0500

    Update README.md

--- deployment/terraform/bootstrap/modules/environment/main.tf
+++ deployment/terraform/bootstrap/modules/environment/main.tf
@@ -8,6 +8,7 @@ locals {
     "roles/iam.serviceAccountCreator",
     "roles/iam.serviceAccountUser",
     "roles/pubsub.admin",
+    "roles/storage.admin"
   ]
 
   compute_sa_email = "${data.google_project.project.number}-compute@developer.gserviceaccount.com"
